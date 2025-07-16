c6b9c79f73d9f79d6acc900b8455f658b03cc446
kammouti <kammmouti@gmail.com>
2025-07-16 15:13:10 +0100

    remove app.run from app.py

--- app.py
+++ app.py
@@ -755,6 +755,6 @@ def pointerinsert():
 
 
 
-if __name__ == "__main__":
-    # app.run(debug=True)
-    app.run(host='0.0.0.0', port=8000, debug=True)
+# if __name__ == "__main__":
+#     # app.run(debug=True)
+#     app.run(host='0.0.0.0', port=8000, debug=True)
