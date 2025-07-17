828a1cce1a86e8bb47db2f07808dc8093cecda5d
kammouti <kammmouti@gmail.com>
2025-07-17 11:09:16 +0100

    bind line delete from gunicorn config . py

--- app.py
+++ app.py
@@ -756,6 +756,6 @@ def pointerinsert():
 
 
 
-# if __name__ == "__main__":
-#     # app.run(debug=True)
-#     app.run(host='0.0.0.0', port=8000, debug=True)
+if __name__ == "__main__":
+    # app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,4 +1,3 @@
-bind = "0.0.0.0:80"
 workers = 3
 worker_class = "sync"
 timeout = 120
